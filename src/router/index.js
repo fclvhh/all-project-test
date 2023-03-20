040f375da98f0e41a727a40f6843ba0859bdca3d
fcdada <374822251@qq.com>
2023-03-20 10:57:12 +0800

    配置主布局路由和挂载点

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MainLayout from "../views/MainLayout";
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name:'MainLayout',
+    component: MainLayout
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+
+
 ]
 
 const router = new VueRouter({
